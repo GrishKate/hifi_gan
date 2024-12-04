2bc17447aeb2ca5cd8fa7bb526c1fc26d9a7310e
Ekaterina Grishina <grishinaekateina@gmail.com>
2024-12-04 20:42:48 +0300

    added readme

---
+++ download_weights.py
@@ -0,0 +1,10 @@
+import gdown
+
+
+def download():
+    gdown.download(id="1evjFar_CgqhIsFnKJnTtXJzRqNz_f8ai")
+    print('Successfully downloaded weights!')
+
+
+if __name__ == "__main__":
+    download()
